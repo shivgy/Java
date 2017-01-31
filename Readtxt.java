5b3acb9e3e03c511d05617d1287f59b8a5c117d3
Shiv Gopal Yadav <shivgy@gmail.com>
2017-01-31 17:43:11 +0530

    Update Readtxt.java

--- Readtxt.java
+++ Readtxt.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class ReadTxt {
-	final static String FILE_NAME = "C:\\Users\\Gopal\\Desktop\\infosys.txt";
+	final static String FILE_NAME = "C:\\Users\\...\\infosys.txt";
 	 final static Charset ENCODING = StandardCharsets.UTF_8;
 	//static Path path = Paths.get(FILE_NAME);
 	
@@ -17,23 +17,30 @@ public class ReadTxt {
 	}
 	
 	public void readFile(String aFileName)throws IOException{
+		Set mySet = new HashSet<>();
+		
 		Path path = Paths.get(aFileName);
 		try(Scanner sc = new Scanner(path,ENCODING.name())){
 			while(sc.hasNextLine()){
 				String str = sc.nextLine();
 				
-				/*ERM Device*/
-				if(str.contains("$SLU")){
-					str = str.substring(str.indexOf("$SLU"), str.indexOf("#"));
-					System.out.println(str.substring(4,19));
-					//System.out.println(str);
+				/*ABC Device*/
+				if(str.contains("$ABC")){
+					str = str.substring(str.indexOf("$XYZ"), str.indexOf("|#"));
+					str = str.substring(str.length()-15, str.length());
+					mySet.add(str);
 				}
-				//SUN100 Device
+				//XYZ Device
 				if(str.contains("Recieved packet:")){
-					str = str.substring(str.indexOf("$SUN"), str.indexOf("|#"));
+					str = str.substring(str.indexOf("$XYZ"), str.indexOf("|#"));
 					System.out.println(str.substring(str.length()-15, str.length()));
 				}
 			}
+			System.out.println(mySet);
+		}
+		catch(Exception e){
+			System.out.println(e.toString());
+			System.out.println(mySet);
 		}
 	}
 }

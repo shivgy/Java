ebcaa77dd9a0a3d2513680356b47cd7d510f257e
Shiv Gopal Yadav <shivgy@gmail.com>
2017-02-18 11:08:50 +0530

    Create CountRepeatition.java

---
+++ Interview/src/CountRepeatition.java
@@ -0,0 +1,44 @@
+/**
+ * Input: aaaabbbffcaa
+ * Output: 4a3b2fc2a
+ */
+
+import java.util.Scanner;
+
+public class CountRepeatition{
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		int count =1;
+		char c = input.charAt(0);
+		String temp = null;
+		String finalString = "";
+		
+		for(int i = 1; i < input.length(); i++){
+			
+			if(c == input.charAt(i)){
+				count++;
+				
+			}else{
+				if(count > 1){
+					temp = String.valueOf(count)+String.valueOf(c);
+				}else{
+					temp = String.valueOf(c);
+				}
+				count = 1;
+				c = input.charAt(i);
+				finalString = finalString+temp;
+			}
+			
+		}
+		if(count > 1){
+			temp = String.valueOf(count)+String.valueOf(c);
+		}else{
+			temp = String.valueOf(c);
+		}
+		finalString += temp;
+		System.out.println(finalString);
+		
+	}
+}

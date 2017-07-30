c624612afcfc157aa5c0ddeff8a3907e55ba9a57
Shiv Gopal Yadav <shivgy@gmail.com>
2017-07-30 06:40:29 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,8 +1,12 @@
 public class BinarySearch{
   public static void main(String[] args){
     int[] arr = {2,5,67,88,93,101,652};
+    
+    Boolean result = binarySearchIT(arr,45);
+    System.out.printl(result.toString());
   }
   
+  /*This method is iterative and works for sorted array only*/
   public static Boolean binarySearchIT(int[] arr, int num){
     int low = 0;
     int high = arr.length - 1;
@@ -17,4 +21,18 @@ public class BinarySearch{
     }
     return false;
   }
+  
+  /*Recursive method*/
+  public static Boolean binarySearchRec(int[] arr, int low, int high, int num){
+    int mid = low + (high - low)/2;
+    while(low <= high){
+      if(arr[mid] == num) return true;
+      else if(arr[mid] < num)
+        binarySearchRec(arr,low,mid-1,num);
+      else
+        binarySearchRec(arr,mid+1,high,num);
+    }
+    return false;
+  }
+  
 }

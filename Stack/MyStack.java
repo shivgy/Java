56135c6a716d556052057d2c638b1cd9945966e1
Shiv Gopal Yadav <shivgy@gmail.com>
2017-08-12 23:35:26 +0530

    Create MyStack.java

---
+++ Stack/MyStack.java
@@ -0,0 +1,33 @@
+public class MyStack<T>{
+  
+  //sub-class
+  private static class StackNode<T>{
+    private T data;
+    private StackNode<T> next;
+    
+    //Constructor
+    public StackNode(T data){
+      this.data = data;
+    }
+  }
+  
+  private StackNode<T> top; //A variable declared of data type StackNode
+  
+  public T pop(){
+    if(top == null) throw  new EmptyStackException();
+    T item = top.data;
+    top = top.next;
+    return item;
+  }
+  
+  public void push(T item){
+    StackNode<T> t = new StackNode<T>(item);
+    t.next = top;
+    top = t;
+  }
+  
+  public T peak(){
+    if(top == null) throw  new EmptyStackException();
+    return top.data;
+  }
+}

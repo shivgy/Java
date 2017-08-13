6552e7f98f6d418195e77e4be877b2336ac6fa78
Shiv Gopal Yadav <shivgy@gmail.com>
2017-08-13 22:02:34 +0530

    Update MyQueue.java

--- Stack/MyQueue.java
+++ Stack/MyQueue.java
@@ -1,8 +1,10 @@
 pubilc class MyQueue<T>{
+  //Sub Class
   private static class QueueNode<T>{
     private T data;
     private QueueNode<T> next;
     
+    //Constructor of sub class
     public QueueNode(T data){
       this.data = data;
     }
@@ -10,5 +12,17 @@ pubilc class MyQueue<T>{
   
   private QueueNode<T> first;
   private QueueNode<T> last;
+  
+  public void add(T item){
+    QueueNode<T> t = new QueueNode<T>(item);
+    if(last != null){
+      last.next = t;
+    }
+    last = t;
+    if(first == null){
+      first = last;
+    }
+  }
+  
   
 }

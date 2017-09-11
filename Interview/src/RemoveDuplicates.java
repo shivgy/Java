d3678c11d6f4bb86bd4b0e21294b07f6b1a08537
Shiv Gopal Yadav <shivgy@gmail.com>
2017-09-11 12:35:34 +0530

    Update RemoveDuplicates.java

--- Interview/src/RemoveDuplicates.java
+++ Interview/src/RemoveDuplicates.java
@@ -12,7 +12,6 @@ public class RemoveDuplicates {
 		}
 		System.out.println(list.toString());
 		
-		for(int i = 0; i < list.size(); i++){
 			for(int j = i+1; j < list.size(); j++){
 				if(list.get(i).equals(list.get(j))){
 					count++;
@@ -21,9 +20,8 @@ public class RemoveDuplicates {
 						j--;
 					}
 				}
+				else count = 0;
 			}
-			count = 0;
-		}
 		System.out.println(list.toString());
 	}
 }
